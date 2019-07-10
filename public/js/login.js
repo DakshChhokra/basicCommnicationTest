fb4205f654f98455b7364e4dcc8497b6933222fe
Daksh Chhokra <chhokradaksh+github@gmail.com>
2019-07-10 14:43:17 -0400

    Changed validation for the various forms

---
+++ public/js/login.js
@@ -0,0 +1,17 @@
+function store() {
+	sessionStorage.setItem('username', document.getElementById('username').value);
+	sessionStorage.setItem('identifier', document.getElementById('identifier').value);
+}
+window.onbeforeunload = function(e) {
+	if (document.getElementById('identifier').value == '' || document.getElementById('username').value == '') {
+		e = e || window.event;
+
+		// For IE and Firefox prior to version 4
+		if (e) {
+			e.returnValue = 'Sure?';
+		}
+
+		// For Safari
+		return 'Sure?';
+	}
+};
--- public/js/survey.js
+++ public/js/survey.js
@@ -19,3 +19,22 @@ identifier.setAttribute('value', sessionStorage.getItem('identifier'));
 
 //append to form element that you want .
 document.getElementById('mainForm').appendChild(identifier);
+
+window.onbeforeunload = function(e) {
+	if (
+		document.getElementById('Comms1').value == '' ||
+		document.getElementById('Comms2').value == '' ||
+		document.getElementById('Comms3').value == '' ||
+		document.getElementById('Check').checked == false
+	) {
+		e = e || window.event;
+
+		// For IE and Firefox prior to version 4
+		if (e) {
+			e.returnValue = 'Sure?';
+		}
+
+		// For Safari
+		return 'Sure?';
+	}
+};
